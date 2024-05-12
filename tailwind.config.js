48920750cbdea7665730bb35f1d2e696b937adfb
zulhusni2003 <zulhusnifamile@gmail.com>
2024-05-13 00:11:58 +0800

    Tailwind Portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/**
+ * Configuration for Tailwind CSS.
+ * This file defines custom configurations for Tailwind CSS, such as theme settings,
+ * container options, extended colors, and custom screen sizes.
+ */
+
+module.exports = {
+  content: ["index.html"], // Specifies which files Tailwind should scan for classes
+  theme: {
+    container: {
+      center: true, // Centers the content horizontally within the container
+      padding: "16px", // Sets padding for container elements
+    },
+    extend: {
+      colors: {
+        primary: "#14b8a6", // Custom primary color
+        secondary: "#64748b", // Custom secondary color
+        dark: "#0f172a", // Custom dark color
+      },
+      screens: {
+        "2xl": "1320px", // Custom screen size definition for 2xl breakpoint
+      },
+    },
+  },
+  plugins: [], // Additional plugins can be added here if needed
+};
